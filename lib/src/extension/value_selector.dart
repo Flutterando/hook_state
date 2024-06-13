21ca918171831e41038c18c1179abebe52bfbfaa
Jacob Moura <jacobaraujo7@gmail.com>
2024-06-12 22:58:49 -0300

    fix format

--- lib/src/extension/value_selector.dart
+++ lib/src/extension/value_selector.dart
@@ -1,4 +1,6 @@
-n on HookState {
+import '../../hook_state.dart';
+
+extension ValueSelectorExtension on HookState {
   T useValueSelector<T>(Selectable<T> selector) {
     useListenable<Selectable<T>>(selector);
     return selector.value;
